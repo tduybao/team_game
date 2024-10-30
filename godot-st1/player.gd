9fd9a02aebfcc861dd8da0a2bbdd28473f14d6d1
ChaosKiz-2005 <xx512sizxx@gmail.com>
2024-10-30 20:07:49 +0700

    Add files via upload

---
+++ godot-st1/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+const SPEED = 1200.0
+const JUMP_VELOCITY = -2700.0
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta * 7
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("default")
+		else:
+			animated_sprite.play("Run_bitch")
+	else:
+		animated_sprite.play("jump")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
